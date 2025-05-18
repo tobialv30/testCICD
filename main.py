3d4de1cc331dc42953dd7591f5d36bfa8cee4911
Tobias Alvarez <tobiasjoaquin@hotmail.com>
2025-05-18 15:13:06 -0300

    commit cambios importantes

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-# src/main.py
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
 
 def sumar(a, b):
     return a + b
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    resultado = None
+    if request.method == 'POST':
+        try:
+            a = float(request.form['a'])
+            b = float(request.form['b'])
+            resultado = sumar(a, b)
+        except ValueError:
+            resultado = 'Por favor, ingresa números válidos.'
+    return render_template('index.html', resultado=resultado)
+
+if __name__ == '__main__':
+    app.run()
+
 
